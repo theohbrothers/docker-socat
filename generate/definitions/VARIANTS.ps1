c4c77abfa7b79754591b6bf31a42590534834102
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-09-25 16:06:31 +0000

    Enhancement (ci): Add support for incremental variants

--- generate/definitions/VARIANTS.ps1
+++ generate/definitions/VARIANTS.ps1
@@ -1,14 +1,20 @@
+$local:VERSIONS = @(
+    '0.0.0'
+)
 $VARIANTS = @(
-    @{
-        # Metadata object
-        _metadata = @{
-            distro = 'alpine'
-            distro_version = '3.12'
-            platforms = 'linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/s390x'
+    foreach ($v in $local:VERSIONS) {
+        @{
+            # Metadata object
+            _metadata = @{
+                distro = 'alpine'
+                distro_version = '3.12'
+                platforms = 'linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/s390x'
+                job_group_key = $v
+            }
+            # Docker image tag. E.g. '3.12'
+            tag = '3.12'
+            tag_as_latest = $true
         }
-        # Docker image tag. E.g. '3.12'
-        tag = '3.12'
-        tag_as_latest = $true
     }
 )
 
