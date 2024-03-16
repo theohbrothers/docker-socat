ab1f18c81c5cc902091b34ea9b61e753707cf876
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2024-03-16 09:22:14 +0000

    Docs (readme): Update GitHub CI badge
    Signed-off-by: Leonard Jonathan Oh <leonardjonathanoh@live.com>

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -1,7 +1,7 @@
 @"
 # docker-socat
 
-[![github-actions](https://github.com/theohbrothers/docker-socat/workflows/ci-master-pr/badge.svg)](https://github.com/theohbrothers/docker-socat/actions)
+[![github-actions](https://github.com/theohbrothers/docker-socat/actions/workflows/ci-master-pr.yml/badge.svg?branch=master)](https://github.com/theohbrothers/docker-socat/actions/workflows/ci-master-pr.yml)
 [![github-release](https://img.shields.io/github/v/release/theohbrothers/docker-socat?style=flat-square)](https://github.com/theohbrothers/docker-socat/releases/)
 [![docker-image-size](https://img.shields.io/docker/image-size/theohbrothers/docker-socat/latest)](https://hub.docker.com/r/theohbrothers/docker-socat)
 
